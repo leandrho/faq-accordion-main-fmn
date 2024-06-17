856308a75a552e09ec122c7c95410bfea2bea189
Leandro Ruiz <lean@localhost.localdomain>
2024-06-16 23:56:22 -0300

    Add index, styles and js

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+const titles = document.querySelectorAll(".item__title");
+titles.forEach((t)=>{
+    const header = t.closest('.item__header');
+    const checkbox = header.querySelector('.checkbox');
+    t.addEventListener("click",()=>{
+        checkbox.checked=!checkbox.checked;
+        checkbox.dispatchEvent(new Event('change'));
+    });
+});
+
+const checkboxes = document.querySelectorAll(".checkbox");
+checkboxes.forEach((checkbox)=>{
+    checkbox.addEventListener('change',()=>{
+        const item = checkbox.closest('.item');
+        item.classList.toggle("active");
+    });
+});
